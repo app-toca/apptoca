930e3aacd50b2953407837fe68f664ebbbee4342
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-09-06 16:05:45 -0300

    feat: config tests

---
+++ src/interfaces/users/index.ts
@@ -0,0 +1,13 @@
+
+export interface IUserRequest {
+    name: string
+    surname: string
+    email: string
+    age: number
+    password: string
+    year: number
+    course: string
+    phrase: string
+    isAdm: boolean
+    img: string
+}
