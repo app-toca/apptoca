4ee78beca93aacb1a848af8e1c8a2cd97e938ee6
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-09-08 14:41:03 -0300

    feat: tests of users routes

---
+++ src/__tests__/mocks/users.ts
@@ -0,0 +1,107 @@
+import { IUserLoginRequest, IUserRequest } from "../../interfaces/users";
+import { IOrganizationRequest } from "../../interfaces/organizations";
+
+export const adminUser: IUserRequest = {
+  name: "Ana",
+  nickname: "Cisne",
+  email: "anavitoriacisn314@gmail.com",
+  age: 22,
+  password: "senhaFortíssima123",
+  year: 2,
+  course: "ESPN",
+  phrase: "Nenhum obstáculo é grande demais para quem desiste",
+  img: "https://thumbs.dreamstime.com/b/love-coding-girl-women-hand-drawn-illustration-cute-monster-love-coding-girl-women-hand-drawn-illustration-cute-monster-144914862.jpg",
+};
+
+export const userAdminLogin: IUserLoginRequest = {
+    email: "anavitoriacisn314@gmail.com",
+    password: "senhaFortíssima123",
+};
+
+export const nonAdminUser: IUserRequest = {
+  name: "João",
+  nickname: "Porto",
+  email: "joao.porto.ismart@gmail.com",
+  age: 23,
+  password: "senhaNãoTãoForte123",
+  year: 3,
+  course: "RBD",
+  phrase: "Não deixe para fazer hoje aquilo que pode procrastinar amanhã",
+  img: "https://thumbs.dreamstime.com/b/coding-fun-hand-drawn-vector-illustration-cute-programmer-cartoon-minimalism-style-cap-party-coding-fun-hand-drawn-157572614.jpg",
+};
+
+export const userNonAdminLogin: IUserLoginRequest = {
+    email: "joao.porto.ismart@gmail.com",
+    password: "senhaNãoTãoForte123",
+};
+
+export const userOwner: IUserRequest = {
+
+  name: "Ana",
+  nickname: "Cisne",
+  email: "anavitoriacisn@gmail.com",
+  age: 22,
+  password: "senhaFortíssima12",
+  year: 2,
+  course: "ESPN",
+  phrase: "Nenhum obstáculo é grande demais para quem desiste",
+  is_owner: true,
+  img: "https://thumbs.dreamstime.com/b/love-coding-girl-women-hand-drawn-illustration-cute-monster-love-coding-girl-women-hand-drawn-illustration-cute-monster-144914862.jpg",  
+}
+
+export const userOwnerLogin: IUserLoginRequest = {
+    email: "anavitoriacisn@gmail.com",
+    password: "senhaFortíssima12",
+};
+
+export const userDifferentEmail: IUserRequest = {
+  name: "João",
+  nickname: "Porto",
+  email: "joao.ismart@gmail.com",
+  age: 23,
+  password: "senhaNãoTãoForte123",
+  year: 3,
+  course: "RBD",
+  phrase: "Não deixe para fazer hoje aquilo que pode procrastinar amanhã",
+  img: "https://thumbs.dreamstime.com/b/coding-fun-hand-drawn-vector-illustration-cute-programmer-cartoon-minimalism-style-cap-party-coding-fun-hand-drawn-157572614.jpg",
+};
+
+export const userDifferentEmailLogin : IUserLoginRequest = {
+  email: "joao.ismart@gmail.com",
+  password: "senhaNãoTãoForte123"
+
+}
+
+export const userOfUnknowOrg: IUserRequest = {
+  name: "Joãozin",
+  nickname: "Porto",
+  email: "joao.porto.isNotSmart@mail.com",
+  age: 23,
+  password: "senhaNãoTãoForte13",
+  year: 3,
+  course: "RBD",
+  phrase: "Não deixe para fazer hoje aquilo que pode procrastinar amanhã",
+  img: "https://thumbs.dreamstime.com/b/coding-fun-hand-drawn-vector-illustration-cute-programmer-cartoon-minimalism-style-cap-party-coding-fun-hand-drawn-157572614.jpg",
+};
+
+export const userToBeDeteleted: IUserRequest = {
+  name: "Joãozin",
+  nickname: "Porto",
+  email: "joao.isNotSmart@mail.com",
+  age: 23,
+  password: "senhaNãoTãoForte13",
+  year: 3,
+  course: "RBD",
+  phrase: "Não deixe para fazer hoje aquilo que pode procrastinar amanhã",
+  img: "https://thumbs.dreamstime.com/b/coding-fun-hand-drawn-vector-illustration-cute-programmer-cartoon-minimalism-style-cap-party-coding-fun-hand-drawn-157572614.jpg",
+};
+
+export const organizationToca: IOrganizationRequest = {
+  name: "Toca",
+  password: "Toca-0912",
+};
+
+export const organizationUnknow: IOrganizationRequest = {
+  name: "Unknow",
+  password: "unk-0913",
+};
--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,13 +1,36 @@
 
 export interface IUserRequest {
     name: string
-    surname: string
+    nickname: string
     email: string
     age: number
     password: string
     year: number
     course: string
     phrase: string
-    isAdm: boolean
-    img: string
+    is_owner?: boolean
+    img?: string
+}
+
+export interface IUserResponse {
+    id: string
+    name: string
+    email: string
+    nickname: string
+    age: number
+    year: number
+    course: string
+    phrase: string
+    is_adm: boolean
+    is_owner: boolean
+    is_active: boolean
+    img?: string
+    created_at: Date
+    updated_at: Date
+    organization_id: string
+}
+
+export interface IUserLoginRequest {
+    email: string
+    password: string
 }
