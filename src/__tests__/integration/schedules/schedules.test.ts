0161b5d3652bf8268e46e12d6c261e6106384176
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-09-09 07:24:11 -0300

    feat: tests of patch and delete of schedule route

--- src/__tests__/integration/schedules/schedules.test.ts
+++ src/__tests__/integration/schedules/schedules.test.ts
@@ -1,35 +1,3 @@
-/*
-
-GET - /schedules/areas/:area_id => all schedules by area
-acesso : admin
-
-GET - /schedules/days/:day[0-6] => schedule by day
-acesso : admin
-return array of users
-
-GET - /schedules/hours/days/:day[0-6]/:hour => schedule by day and hour
-acesso : admin
-return array of users
-
-GET - /schedules/:area_id/report => report of hours by quantity of users
-[ 
-{day: 0, hour:'20:00', qttUsers: 3},
-{day: 2, hour:'21:00', qttUsers: 2}
-]
-
-acesso : admin
-!!retornar em ordem decrescente por qttUsers
-
-PATCH - /schedules => update all schedules of user
-acesso : todo usuario logado
-!!busca pelo id do usuário
-
-DELETE - /schedules/:schedule_id => delete an schedule
-acesso : proprio usuario
-!!busca pelo id do usuário
-
-*/
-
 import { DataSource } from "typeorm";
 import AppDataSource from "../../../data-source";
 import request from "supertest"
@@ -90,7 +58,7 @@ describe("/schedules", () => {
     })
 
     const scheduleNonAdminUser = [{name: 1, hour:'20:00'}, {name: 0, hour:'21:00'}]
-    const scheduleAdminUser = [{name: 1, hour:'19:00'}, {name: 2, hour:'17:00'}]
+    const scheduleAdminUser = [{name: 1, hour:'20:00'}, {name: 2, hour:'17:00'}]
 
     test("POST /schedules -  Must be able to add a schedule",async () => {
     
@@ -183,7 +151,7 @@ describe("/schedules", () => {
     
     })
 
-    test("GET /schedules -  should not be able to list all schedules if being non admin",async () => {
+    test("GET /schedules -  Should not be able to list all schedules if being non admin",async () => {
     
         const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
         const response = await request(app).get(`/schedules`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
@@ -192,10 +160,110 @@ describe("/schedules", () => {
         expect(response.body).toHaveProperty("message")    
     
     })
+
+
+test("GET /schedules/areas/:area_id-  Must be able to list schedules of users in some area if being admin",async () => {
     
+    const adminLoginResponse = await request(app).post("/login").send(userOwnerLogin);
+    const responseAreaUser = await request(app).post('/administration/area').send({user_id: userNonAdminCreated.id, area_id: marketingAreaCreated.id}).set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+    const response = await request(app).get(`/schedules/areas/${marketingAreaCreated.id}`).set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
 
+    expect(response.body).toHaveLength(2)
+    expect(response.status).toBe(200)
+    expect(response.body[0]).toHaveProperty("id")
+    expect(response.body[0]).toHaveProperty("user_id") 
+    expect(response.body[0].user_id).toBe(userNonAdminCreated.id)    
+    expect(response.body[0]).toHaveProperty("day_id")    
+    expect(response.body[0]).toHaveProperty("hour_id")    
 
+})
 
+test("GET /schedules/areas/:area_id - Should not be able to list schedules of users in some area if not being admin",async () => {
+    
+    const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
+    const adminLoginResponse = await request(app).post("/login").send(userOwnerLogin);
+    const responseAreaUser = await request(app).post('/administration/area').send({user_id: userOwnerCreated.id, area_id: marketingAreaCreated.id}).set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+    const response = await request(app).get(`/schedules/areas/${marketingAreaCreated.id}`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
 
+    
+    expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty("message")
+})
+
+test("GET /schedules/hours/days/areas/:day[0-6]/:hour/:area_id -  Must be able to list users in some area that have some schedule if being admin",async () => {
+    
+    const adminLoginResponse = await request(app).post("/login").send(userOwnerLogin);
+    const response = await request(app).get(`/schedules/hours/days/areas/0/20:00/${marketingAreaCreated.id}`).set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+
+    expect(response.body).toHaveLength(2)
+    expect(response.status).toBe(200)
+    expect(response.body[0]).toHaveProperty("id")
+    expect(response.body[0]).toHaveProperty("name")       
+    expect(response.body[0]).toHaveProperty("email")    
+
+})
+
+test("GET /schedules/hours/days/areas/:day[0-6]/:hour/:area_id - Should not be able to list users in some area that have some schedule if not being admin",async () => {
+    
+    const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
+    const response = await request(app).get(`/schedules/hours/days/areas/0/20:00/${marketingAreaCreated.id}`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
+
+    
+    expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty("message")
+})
+
+test("GET /schedules/:area_id/report -  Must be able to get the report of hours by quantity of users if being admin",async () => {
+    
+    const adminLoginResponse = await request(app).post("/login").send(userOwnerLogin);
+    const response = await request(app).get(`/schedules/${marketingAreaCreated.id}/report`).set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+
+    expect(response.body).toHaveLength(3)
+    expect(response.status).toBe(200)
+    expect(response.body[0]).toHaveProperty("day")
+    expect(response.body[0]).toHaveProperty("hour")       
+    expect(response.body[0]).toHaveProperty("qtt_users")
+    expect(response.body[0].qtt_users).toBe(2)
+
+})
+
+test("GET /schedules/:area_id/report - Should not be able to to get the report of hours by quantity of users if not being admin",async () => {
+    
+    const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
+    const response = await request(app).get(`/schedules/${marketingAreaCreated.id}/report`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
+
+    
+    expect(response.status).toBe(401)
+    expect(response.body).toHaveProperty("message")
+})
+
+const scheduleNonAdminUserChanged = [{name: 4, hour:'21:00'}, {name: 5, hour:'16:00'}, {name: 3, hour:'18:00'}]
+
+test("PATCH /schedules -  Must be able to change a schedule",async () => {
+    
+    const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
+    const response = await request(app).patch(`/schedules`).send(scheduleNonAdminUserChanged).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
+
+    expect(response.body).toHaveLength(3)
+    expect(response.status).toBe(200)
+    expect(response.body[0]).toHaveProperty("id")
+    expect(response.body[0]).toHaveProperty("user_id") 
+    expect(response.body[0].user_id).toBe(userNonAdminCreated.id)    
+    expect(response.body[0]).toHaveProperty("day_id")    
+    expect(response.body[0]).toHaveProperty("hour_id")  
+
+})  
+
+test("DELETE /schedules -  Must be able to delete all schedule",async () => {
+    
+    const nonAdminLoginResponse = await request(app).post("/login").send(userNonAdminLogin);
+    const response = await request(app).delete(`/schedules`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
+    const responseSchedulesByThisUser = await request(app).get(`/schedules/users/${userNonAdminCreated.id}`).set("Authorization", `Bearer ${nonAdminLoginResponse.body.token}`)
+    
+    expect(responseSchedulesByThisUser.body).toHaveLength(0)
+    expect(response.status).toBe(204)
+     
+
+})
 
 })
