5d9011e179c33b9625523f3a38855f7006ad6d61
Pedro Lucas Xavier <pedrolucas.xs@gmail.com>
2022-09-09 10:23:01 -0300

    fix: controllers folders fixed on meetings and schedules

--- src/controllers/meetings/listAllMeetings.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { listAllMeetingsService } from "../../services/meetings/listAllMeetings.service";
-
-export const listAllMeetingsController = async (req: Request, res: Response) => {
-
-    const meetings = await listAllMeetingsService()
-
-    return res.status(200).json(meetings)
-}
