42815ce1e5896fef782f94072675ade18eecd6d5
GusBTB <gustavocs81@gmail.com>
2022-09-11 18:57:08 -0300

    fix: isOwner and checkorg middleware

--- src/middlewares/isOwner.middleware.ts
+++ src/middlewares/isOwner.middleware.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "../error/global";
 
 const isOwnerMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const { is_owner } = req.user;
 
   if (!is_owner) {
-    return res.status(403).json({
-      message: "Você não tem permissão",
-    });
+    throw new AppError(401, "Unauthorizated");
   }
 
   next();
