9c496e9ed3c745e9c8de3a9c68e12beea3a43d25
GusBTB <gustavocs81@gmail.com>
2022-09-12 13:50:15 -0300

    fix: meetings routes instance to plain n controller

--- src/controllers/meetings.controllers.ts
+++ src/controllers/meetings.controllers.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 import { createMeetingService } from "../services/meetings/createMeeting.service";
 import { deleteMeetingService } from "../services/meetings/deleteMeeting.service";
@@ -8,75 +9,77 @@ import { updateMeetingService } from "../services/meetings/updateMeeting.service
 
 //Cria uma nova meeting
 
-export const createMeetingController = async(req: Request, res: Response) => {
+export const createMeetingController = async (req: Request, res: Response) => {
+  const { area_id } = req.params;
 
-    const { area_id } = req.params;
+  const { description, duration, ata, date_time } = req.body;
 
-    const { description, duration, ata, date_time } = req.body;
+  const user_id = req.body.id;
 
-    const user_id = req.body.id;
+  const meeting = await createMeetingService({
+    area_id,
+    ata,
+    date_time,
+    description,
+    duration,
+    user_id,
+  });
 
-    const meeting = await createMeetingService({ area_id, ata, date_time, description, duration, user_id });
-
-    return res.status(201).json(meeting);
-
-}
+  return res.status(201).json(instanceToPlain(meeting));
+};
 
 //Deleta uma meeting
 
-export const deleteMeetingController = async(req: Request, res: Response) => {
+export const deleteMeetingController = async (req: Request, res: Response) => {
+  const { meeting_id } = req.params;
 
-    const { meeting_id } = req.params;
+  const message = await deleteMeetingService({ meeting_id });
 
-    const message = await deleteMeetingService({ meeting_id });
-
-    return res.status(200).json({ message: message })
-
-}
+  return res.status(200).json({ message: message });
+};
 
 //Lista todas as meetings
 
-export const listAllMeetingsController = async (req: Request, res: Response) => {
+export const listAllMeetingsController = async (
+  req: Request,
+  res: Response
+) => {
+  const meetings = await listAllMeetingsService();
 
-    const meetings = await listAllMeetingsService()
-
-    return res.status(200).json(meetings)
-}
+  return res.status(200).json(meetings);
+};
 
 //Lista todas as meetings de uma área
 
-export const listMeetingsByAreaController = async (req: Request, res: Response) => {
+export const listMeetingsByAreaController = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const meetings = await listMeetingsByAreaService({ id });
 
-    const meetings = await listMeetingsByAreaService({ id });
-
-    return res.status(200).json(meetings);
-
-}
+  return res.status(200).json(meetings);
+};
 
 //Lista uma meeting específica
 
 export const listOneMeetingController = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
+  const meeting = await listOneMeetingService({ id });
 
-    const meeting = await listOneMeetingService({ id });
-
-    return res.status(200).json(meeting)
-
-}
+  return res.status(200).json(meeting);
+};
 
 //Atualiza uma meeting
 
-export const updateMeetingController = async(req: Request, res: Response) => {
+export const updateMeetingController = async (req: Request, res: Response) => {
+  const { description } = req.body;
 
-    const { description } = req.body;
+  const { meeting_id } = req.params;
 
-    const { meeting_id } = req.params;
+  const meeting = await updateMeetingService({ meeting_id, description });
 
-    const meeting = await updateMeetingService({ meeting_id, description });
-
-    return res.status(200).json(meeting);
-
-}
+  return res.status(200).json(meeting);
+};
