a8ce28bc3672b8d90af003716bbdd52a1994f6ac
João Pedro Porto Teixeira <joao.porto.ismart@gmail.com>
2022-09-12 20:04:45 -0300

    fix: comments mock

--- src/__tests__/mocks/comments.ts
+++ src/__tests__/mocks/comments.ts
@@ -3,7 +3,6 @@ import { ICommentTest } from "../../interfaces/comments";
 export const comment: ICommentTest = {
   content: "Gostei do post, ta iluminando bem",
 };
-<<<<<<< HEAD
 
 export const comment2: ICommentTest = {
   content: "Eu discordo totalmente",
@@ -12,5 +11,3 @@ export const comment2: ICommentTest = {
 export const commentUpdate: ICommentTest = {
   content: "Mudei de ideia, gostei não",
 };
-=======
->>>>>>> dfe0f57299abcb5dc46805797268c555567acb28
