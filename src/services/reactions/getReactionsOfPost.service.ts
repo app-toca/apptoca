ce57afde53ee8b2b5ce59d9e61cc442b5af3dc2d
Gustavo da Costa <gustavocosta.realitydeveloper@gmail.com>
2022-09-13 15:36:47 -0400

    fix: return of getReactions

--- src/services/reactions/getReactionsOfPost.service.ts
+++ src/services/reactions/getReactionsOfPost.service.ts
@@ -1,37 +1,34 @@
-import AppDataSource from "../../data-source"
-import { Posts } from "../../entities/Posts.entity"
-import { Reaction } from "../../entities/Reactions.entity"
-import { AppError } from "../../error/global"
-
-
+import AppDataSource from "../../data-source";
+import { Posts } from "../../entities/Posts.entity";
+import { Reaction } from "../../entities/Reactions.entity";
+import { AppError } from "../../error/global";
 
 const getReactionsOfPostService = async (post_id: string) => {
+  const postsRepository = AppDataSource.getRepository(Posts);
+  const reactionsRepository = AppDataSource.getRepository(Reaction);
 
-    const postsRepository = AppDataSource.getRepository(Posts)
-    const reactionsRepository = AppDataSource.getRepository(Reaction)
+  const post = postsRepository.findOneBy({
+    id: post_id,
+  });
 
-    const post = postsRepository.findOneBy({
-        id: post_id
-    })
+  if (!post) {
+    throw new AppError(404, "Post not found");
+  }
 
-    if(!post) {
+  const reactions = await reactionsRepository.find({
+    where: {
+      post: {
+        id: post_id,
+      },
+    },
+    relations: {
+      post: true,
+    },
+  });
 
-        throw new AppError(404, "Post not found")
-    }
+  const filterReaction = reactions.map((reaction) => reaction.post.reactions);
 
-    const reactions = await reactionsRepository.find({
-        where: {
-            post: {
-                id: post_id
-            }
-        }, relations: {
-            post: true
-        }
+  return filterReaction[0];
+};
 
-    })
-
-    return reactions
-
-}
-
-export default getReactionsOfPostService
+export default getReactionsOfPostService;
