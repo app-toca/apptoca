6ed1795612d49e15abe6ab5d5516ca83cde10dc5
GusBTB <gustavocs81@gmail.com>
2022-09-12 22:11:37 -0300

    fix: administration routes and check org middleware

--- src/services/users/areasUser.service.ts
+++ src/services/users/areasUser.service.ts
@@ -4,9 +4,13 @@ import { Area_users } from "../../entities/Area_users.entity";
 import { User } from "../../entities/User.entity";
 import { AppError } from "../../error/global";
 
-export const areasUserService = async (user_id: string, is_adm: boolean, id: string) => {
+export const areasUserService = async (
+  user_id: string,
+  is_adm: boolean,
+  id: string
+) => {
   const usersRepository = AppDataSource.getRepository(User);
-  const areaUsersRepository = AppDataSource.getRepository(Area_users)
+  const areaUsersRepository = AppDataSource.getRepository(Area_users);
 
   const findUser = await usersRepository.findOneBy({
     id: user_id,
@@ -16,23 +20,20 @@ export const areasUserService = async (user_id: string, is_adm: boolean, id: str
     throw new AppError(400, "Invalid Id");
   }
 
-
   if (!is_adm && id !== findUser?.id) {
     throw new AppError(403, "Invalid User");
-  } 
+  }
 
   const areasUser = await areaUsersRepository.find({
     where: {
-      user: findUser
-    }
-  })
+      user: { id: user_id },
+    },
+  });
 
   const areas = areasUser.map((el) => {
-    const {area, ...rest} = el
-    return area
-  })
+    const { area, ...rest } = el;
+    return area;
+  });
 
-  return areas
-
-
+  return areas;
 };
